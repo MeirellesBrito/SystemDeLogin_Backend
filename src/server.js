0b474c8a7d3748e3c9d890d09bcdb8154ef31636
MeirellesBrito <aluno.f.meirelles@gmail.com>
2024-03-19 08:14:07 -0300

    FOI CRIADO UMA PASTA ROUTES.JS E FOI COLOCADO API DO ROUTES DE /LOGIN

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const routes = require('./routes');
 const app = express();
 
 const PORT = 3001;
 
 app.use(express.json());
+app.use(routes);
+
 app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
