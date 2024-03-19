5ef5af0d452ee0f1cdb73971e18ab93e56e826d1
MeirellesBrito <aluno.f.meirelles@gmail.com>
2024-03-19 08:30:40 -0300

    FOI COLOCADO AS AVALIAÇÕES DE USUARIOS

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.post('/login',(req,res) => {
-    res.send("Login endpont");
-})
+const users = [
+  {
+    id: 1,
+    name: "Fernando",
+    email: "test@gmail.com",
+    passoword: "123",
+  },
+];
+
+routes.post("/login", (req, res) => {
+  const { email, passoword } = req.body;
+  const user = users.find(
+    (user) => user.email === email && user.passoword === passoword
+  );
+
+  if (user) {
+    return res.status(200).json(user);
+  }
+  return res.status(401).json({ message: "Credencias invalida" });
+});
 
 module.exports = routes;
